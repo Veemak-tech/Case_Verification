d51b46ef2ba4d55c8defc8b9ff881f45f77a15df
skarthi <74171339+skarthiveemak@users.noreply.github.com>
2021-01-11 16:01:13 +0530

    master

--- caseVerification-UI/src/app/components/users/user-edit/user-edit.component.ts
+++ caseVerification-UI/src/app/components/users/user-edit/user-edit.component.ts
@@ -3,11 +3,11 @@ import { Router, NavigationExtras } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 
 import { Component, OnInit, EventEmitter,Output } from '@angular/core';
-import { FormControl, FormGroup, Validators,FormsModule } from "@angular/forms";
+import { FormControl, FormGroup, Validators,FormsModule,NgForm } from "@angular/forms";
 import { userListDB } from 'src/app/shared/tables/list-users';
 import {AuthService} from "src/app/services/auth.service";
 import { User} from "src/app/models/User";
-import { NgForm} from '@angular/forms';
+
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
@@ -25,43 +25,42 @@ export class UserEditComponent implements OnInit {
     private router: Router,
     private route:ActivatedRoute
   ) {
-    
-      
+
+
 // const id:number = Number( this.route.snapshot.paramMap.get('id'));
  var id : number;
 
 this.route.queryParams.subscribe(params => {
   id = params['id'];
-  
+
 });
 
     this.user.getDatabyID(id).subscribe((data:User)=>{
       debugger;
       console.warn(data);
-       this.userdetail=data[0];
+      this.userdetail=data[0];
+      this.EditForm= new FormGroup({
+name:new FormControl(data[0]['name']),
+email :new FormControl(data[0]['email']),
+RoleID: new FormControl(data[0]['RoleID']),
+
+
+      })
+
     })
   }
 
   ngOnInit(): void {
-  
-    this.EditForm= this.createFormGroup();
+
+   // this.EditForm= this.createFormGroup();
   }
 
 
 
-  createFormGroup(): FormGroup {
-    return new FormGroup({
-      name: new FormControl("", [Validators.required, Validators.minLength(5)]),
-      email: new FormControl("", [Validators.required, Validators.email]),
-      RoleID: new FormControl("", [Validators.required, Validators.minLength(5)]),
-    });
+  // createFormGroup(): FormGroup {
+  //   return new FormGroup({
+  //     name: new FormControl(data['name'], [Validators.required, Validators.minLength(5)]),
+  //     email: new FormControl(['email'], [Validators.required, Validators.email]),
+  //     RoleID: new FormControl(['RoleID'], [Validators.required, Validators.minLength(5)]),
+    }
 
-}
-update( ): void {
-  this.user.update(this.EditForm.value).subscribe((msg) => {
-    console.log(msg);
-    console.log("user updated");
-  
-  });
-}
-}
