4e235f25705e7c1a979febd502ac09a868b36185
Sam-Krish <74168306+Sam-Krish@users.noreply.github.com>
2021-02-16 08:34:54 +0530

    master

--- caseVerification-UI/src/app/app.module.ts
+++ caseVerification-UI/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+// import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
 import { Ng2SmartTableModule } from 'ng2-smart-table'
 import { DashboardModule } from './components/dashboard/dashboard.module';
 import { SharedModule } from './shared/shared.module';
@@ -65,7 +65,7 @@ import {NgxPaginationModule} from 'ngx-pagination';
     ReactiveFormsModule,
     BrowserModule,
     Ng2SmartTableModule,
-    NgxBootstrapIconsModule.pick(allIcons),
+    //NgxBootstrapIconsModule.pick(allIcons),
     ToastrModule.forRoot(),
     NgxPaginationModule
 
