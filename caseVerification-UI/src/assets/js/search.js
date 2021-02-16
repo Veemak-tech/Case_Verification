143adba81ec4128f48fc80b5ee1a596ddce61856
skarthi <74171339+skarthiveemak@users.noreply.github.com>
2021-02-16 09:28:35 +0530

    master

--- caseVerification-UI/src/assets/js/search.js
+++ caseVerification-UI/src/assets/js/search.js
@@ -5,6 +5,9 @@
   }
 
   var elements = document.getElementsByClassName("column");
+  var elements1 = document.getElementsByClassName("column-m");
+
+
 
   // Declare a loop variable
   var i;
@@ -12,7 +15,13 @@
   // List View
   function listView() {
     for (i = 0; i < elements.length; i++) {
-      elements[i].style.width = "100%";
+      elements[i].style.width = "77%";
+    }
+  }
+// list -m
+  function listView() {
+    for (i = 0; i < elements1.length; i++) {
+      elements1[i].style.width = "100%";
     }
   }
 
@@ -26,7 +35,7 @@
   // List 2 view
       function list2View(){
         for (i=0; i<elements.length; i++){
-          elements[i].style.width = "50%";
+          elements[i].style.width = "42%";
         }
       }
 
@@ -37,6 +46,63 @@
         if (checkboxes[i] != source)
             checkboxes[i].checked = source.checked;
     }
+}
+
+// sort by alphapet
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc";
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
 }
 
   //---------------------------------------------------------
