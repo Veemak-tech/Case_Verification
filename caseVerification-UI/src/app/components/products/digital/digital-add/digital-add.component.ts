8201935021361849b61cae1160671e98e9c51134
Sam-Krish <74168306+Sam-Krish@users.noreply.github.com>
2021-02-24 23:13:38 +0530

    master

--- caseVerification-UI/src/app/components/products/digital/digital-add/digital-add.component.ts
+++ caseVerification-UI/src/app/components/products/digital/digital-add/digital-add.component.ts
@@ -10,7 +10,7 @@ import { CasedetailsService } from './../../../../services/casedetails.service';
 import {JwPaginationModule} from 'jw-angular-pagination'
 import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import { Data } from '@angular/router';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
 
 
 
